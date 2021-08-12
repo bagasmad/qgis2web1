6eb64025defeee132a62306e834b81e55b78c0ae
Bagas Darmawan <bagas.m@mail.ugm.ac.id>
2021-08-12 19:27:28 +0700

    Add files via upload

--- data/RentalMobil_8.js
+++ data/RentalMobil_8.js
@@ -3,6 +3,6 @@ var json_RentalMobil_8 = {
 "name": "RentalMobil_8",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "No": 1.0, "Jenis": "Sarana Pariwisata ", "Kelompok": "Angkutan ", "Desa": "Petta Timur", "Nama": "Rental Mobil ZANDIF (Wachid Bouato)", "E": 125.5595251, "S": 3.6457268 }, "geometry": { "type": "Point", "coordinates": [ 125.5595251, 3.6457268 ] } }
+{ "type": "Feature", "properties": { "No": null, "Jenis": null, "Kelompok": null, "Desa": null, "Nama": "Rental Mobil ZANDIF (Wachid Bouato)", "E": 125.5595251, "S": 3.6457268 }, "geometry": { "type": "Point", "coordinates": [ 125.5595251, 3.6457268 ] } }
 ]
 }
---
+++ data/TempatIbadah_5.js
@@ -0,0 +1,12 @@
+var json_TempatIbadah_5 = {
+"type": "FeatureCollection",
+"name": "TempatIbadah_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "No": null, "Jenis": null, "Kelompok": null, "Desa": null, "Nama": "Gereja GPDI Bethani Petta", "E": 125.5607842, "S": 3.6464267 }, "geometry": { "type": "Point", "coordinates": [ 125.5607842, 3.6464267 ] } },
+{ "type": "Feature", "properties": { "No": null, "Jenis": null, "Kelompok": null, "Desa": null, "Nama": "Masjid Al-Jihad", "E": 125.5597109, "S": 3.6465702 }, "geometry": { "type": "Point", "coordinates": [ 125.5597109, 3.6465702 ] } },
+{ "type": "Feature", "properties": { "No": null, "Jenis": null, "Kelompok": null, "Desa": null, "Nama": "Masjid Thoriq bin ziyad at'takwa embuhanga", "E": 125.5744781, "S": 3.6416228 }, "geometry": { "type": "Point", "coordinates": [ 125.5744781, 3.6416228 ] } },
+{ "type": "Feature", "properties": { "No": null, "Jenis": null, "Kelompok": null, "Desa": null, "Nama": "Masjid ", "E": 125.5659783, "S": 3.647562 }, "geometry": { "type": "Point", "coordinates": [ 125.5659783, 3.647562 ] } },
+{ "type": "Feature", "properties": { "No": null, "Jenis": null, "Kelompok": null, "Desa": null, "Nama": "Mushola ", "E": 125.5622151, "S": 3.6459898 }, "geometry": { "type": "Point", "coordinates": [ 125.5622151, 3.6459898 ] } }
+]
+}
